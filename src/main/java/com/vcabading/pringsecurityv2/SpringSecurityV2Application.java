fae44834ea1b764f2f28c9a744a6a359eb135e36
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-19 00:30:48 -0800

    inital commit

---
+++ src/main/java/com/vcabading/pringsecurityv2/SpringSecurityV2Application.java
@@ -0,0 +1,13 @@
+package com.vcabading.pringsecurityv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vcabading/pringsecurityv2/SpringSecurityV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcabading.pringsecurityv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
