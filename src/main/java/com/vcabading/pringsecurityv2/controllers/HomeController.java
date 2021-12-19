4fdb5a922cf56d6ae49ce55bf71a9fffbf5b73e8
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-19 14:42:34 -0800

    add /registration and /login routes to HomeController

--- src/main/java/com/vcabading/pringsecurityv2/controllers/HomeController.java
+++ src/main/java/com/vcabading/pringsecurityv2/controllers/HomeController.java
@@ -1,7 +1,13 @@
 package com.vcabading.pringsecurityv2.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.vcabading.pringsecurityv2.models.User;
 
 /////////////////////////////////////////////////////////////////
 //HOME CONTROLLER
@@ -13,4 +19,14 @@ public class HomeController {
 	public String index() {
 		return "index.jsp";
 	}
+	
+    @RequestMapping("/registration")
+    public String registerForm(@Valid @ModelAttribute("user") User user) {
+        return "registrationPage.jsp";
+    }
+    
+    @RequestMapping("/login")
+    public String login() {
+        return "loginPage.jsp";
+    }
 }
