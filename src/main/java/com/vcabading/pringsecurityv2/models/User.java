c94b73442692825a4dce822f38eb0ab8e1b8663a
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-19 02:07:42 -0800

    add User Class Model

---
+++ src/main/java/com/vcabading/pringsecurityv2/models/User.java
@@ -0,0 +1,108 @@
+package com.vcabading.pringsecurityv2.models;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.management.relation.Role;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+/** ******************************************************
+ * 	Users Class 
+ */
+
+@Entity
+@Table(name="users")
+public class User {
+    
+	//	**** FIELDS **************************************
+	
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String password;
+    @Transient
+    private String passwordConfirmation;
+    @Column(updatable=false)
+    private Date createdAt;
+    private Date updatedAt;
+    
+    // **** Many-To-Many Relationship ********************
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "users_roles", 
+        joinColumns = @JoinColumn(name = "user_id"), 
+        inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles;
+    
+    //	**** CONSTRUCTORS ********************************
+    
+    public User() {
+    }
+    
+    //	**** GETTERS AND SETTERS *************************
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswordConfirmation() {
+		return passwordConfirmation;
+	}
+
+	public void setPasswordConfirmation(String passwordConfirmation) {
+		this.passwordConfirmation = passwordConfirmation;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
