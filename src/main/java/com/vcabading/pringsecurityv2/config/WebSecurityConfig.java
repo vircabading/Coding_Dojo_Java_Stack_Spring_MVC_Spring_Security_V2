09530a10dafc61966ec8c3abb6c9f2e4a4ef0b44
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-19 14:40:16 -0800

    add WebSecurity Config

---
+++ src/main/java/com/vcabading/pringsecurityv2/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.vcabading.pringsecurityv2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+            authorizeRequests()
+                .antMatchers("/css/**", "/js/**", "/registration").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+    }
+}
