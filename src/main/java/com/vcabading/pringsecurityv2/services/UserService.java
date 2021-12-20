29c1819da63aded77e2d0cf55d716fe1b5b89597
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-19 20:56:15 -0800

    add UserService

--- src/main/java/com/vcabading/pringsecurityv2/services/UserService.java
+++ src/main/java/com/vcabading/pringsecurityv2/services/UserService.java
@@ -1,5 +1,41 @@
 package com.vcabading.pringsecurityv2.services;
 
-public class UserService {
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import com.vcabading.pringsecurityv2.models.User;
+import com.vcabading.pringsecurityv2.repositories.RoleRepository;
+import com.vcabading.pringsecurityv2.repositories.UserRepository;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    
+    public UserService(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder bCryptPasswordEncoder)     {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+    
+    
+    // 1
+    public void saveWithUserRole(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles(roleRepository.findByName("ROLE_USER"));
+        userRepository.save(user);
+    }
+     
+     // 2 
+    public void saveUserWithAdminRole(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles(roleRepository.findByName("ROLE_ADMIN"));
+        userRepository.save(user);
+    }    
+    
+    // 3
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
 }
