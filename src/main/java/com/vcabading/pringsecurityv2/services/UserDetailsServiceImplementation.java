af5d959fb2cd9e1a2547f078a4abb6b3feeccadc
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-19 21:52:56 -0800

    add UserDetailsServiceImplementation

---
+++ src/main/java/com/vcabading/pringsecurityv2/services/UserDetailsServiceImplementation.java
@@ -0,0 +1,46 @@
+package com.vcabading.pringsecurityv2.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.vcabading.pringsecurityv2.models.Role;
+import com.vcabading.pringsecurityv2.models.User;
+import com.vcabading.pringsecurityv2.repositories.UserRepository;
+
+@Service
+public class UserDetailsServiceImplementation implements UserDetailsService {
+    private UserRepository userRepository;
+    
+    public UserDetailsServiceImplementation(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+    // 1
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        
+        if(user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), getAuthorities(user));
+    }
+    
+    // 2
+    private List<GrantedAuthority> getAuthorities(User user){
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        for(Role role : user.getRoles()) {
+            GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(role.getName());
+            authorities.add(grantedAuthority);
+        }
+        return authorities;
+    }
+}
+
