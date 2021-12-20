0b67cfaeb1cae46de659f8eef66f4a98317dc3b0
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-19 21:01:31 -0800

    add /registration post mapping

--- src/main/java/com/vcabading/pringsecurityv2/controllers/HomeController.java
+++ src/main/java/com/vcabading/pringsecurityv2/controllers/HomeController.java
@@ -1,13 +1,19 @@
 package com.vcabading.pringsecurityv2.controllers;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.vcabading.pringsecurityv2.models.User;
+import com.vcabading.pringsecurityv2.services.UserService;
 
 /////////////////////////////////////////////////////////////////
 //HOME CONTROLLER
@@ -15,6 +21,10 @@ import com.vcabading.pringsecurityv2.models.User;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping("/")
 	public String index() {
 		return "index.jsp";
@@ -23,6 +33,15 @@ public class HomeController {
     @RequestMapping("/registration")
     public String registerForm(@Valid @ModelAttribute("user") User user) {
         return "registrationPage.jsp";
+    }
+    
+    @PostMapping("/registration")
+    public String registration(@Valid @ModelAttribute("user") User user, BindingResult result, Model model, HttpSession session) {
+        if (result.hasErrors()) {
+            return "registrationPage.jsp";
+        }
+        userService.saveWithUserRole(user);
+        return "redirect:/login";
     }
     
     @RequestMapping("/login")
